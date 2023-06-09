aa4b692f797caeb713fd63563a345084bf9df620
Elio Bischof <eliobischof@gmail.com>
2023-06-09 14:41:40 +0200

    avoid port conflicts

--- charts/zitadel/test/installation/service_tunnel.go
+++ charts/zitadel/test/installation/service_tunnel.go
@@ -1,6 +1,11 @@
 package installation
 
-import "github.com/gruntwork-io/terratest/modules/k8s"
+import (
+	"context"
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"net"
+)
 
 type CloseFunc func()
 
@@ -11,6 +16,23 @@ func (c CloseFunc) Close() {
 // ServiceTunnel must be closed using the returned close function
 func ServiceTunnel(cfg *ConfigurationTest) func() {
 	serviceTunnel := k8s.NewTunnel(cfg.KubeOptions, k8s.ResourceTypeService, cfg.zitadelRelease, 8080, 8080)
+	awaitPortToBeFree(cfg, 8080)
 	serviceTunnel.ForwardPort(cfg.T())
 	return serviceTunnel.Close
+}
+
+func awaitPortToBeFree(cfg *ConfigurationTest, port int) {
+	t := cfg.T()
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		t.Fatal(err)
+	}
+	Await(cfg.Ctx, t, nil, 60, func(ctx context.Context) error {
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			return err
+		}
+		defer l.Close()
+		return nil
+	})
 }
