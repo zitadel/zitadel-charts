e7d0ee9ebb73f719f4f7481dca8edf8c7a272f64
Elio Bischof <eliobischof@gmail.com>
2023-06-15 09:37:10 +0200

    don't type check kubectl cli errors

--- charts/zitadel/test/installation/setup.go
+++ charts/zitadel/test/installation/setup.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/jinzhu/copier"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"strings"
 )
 
 func (s *ConfigurationTest) SetupTest() {
@@ -18,7 +19,7 @@ apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
   name: crdb
-`); err != nil && !errors.IsNotFound(err) {
+`); err != nil && !isNotFoundFromKubectl(err) {
 		t.Fatal(err)
 	}
 	if err := k8s.KubectlDeleteFromStringE(t, clusterKubectl, `
@@ -26,17 +27,24 @@ apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
   name: crdb
-`); err != nil && !errors.IsNotFound(err) {
+`); err != nil && !isNotFoundFromKubectl(err) {
 		t.Fatal(err)
 	}
 	_, err := k8s.GetNamespaceE(t, s.KubeOptions, s.KubeOptions.Namespace)
-	isNotFound := errors.IsNotFound(err)
-	if err != nil && !isNotFound {
+	notFound := errors.IsNotFound(err)
+	if err != nil && !notFound {
 		t.Fatal(err)
 	}
-	if isNotFound {
+	if notFound {
 		k8s.CreateNamespace(t, s.KubeOptions, s.KubeOptions.Namespace)
 		return
 	}
 	s.log.Logf(s.T(), "Namespace: %s already exist!", s.KubeOptions.Namespace)
+}
+
+func isNotFoundFromKubectl(err error) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(err.Error(), "not found")
 }
