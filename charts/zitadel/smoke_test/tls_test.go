b654867ac3fc372c4c815856c81c2a6c9c8a0002
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-09-30 10:50:36 +0700

    feat: added more tests for the self-signed cert issue as well

--- charts/zitadel/smoke_test/tls_test.go
+++ charts/zitadel/smoke_test/tls_test.go
@@ -1,24 +1,23 @@
-package smoke_test_test
+package smoke_test
 
 import (
-	"context"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net/http"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/stretchr/testify/require"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/zitadel/zitadel-charts/charts/zitadel/smoke_test/support"
 )
 
-func TestTLSWithUserProvidedCertificate(t *testing.T) {
+func TestTLSMatrix(t *testing.T) {
 	t.Parallel()
 
 	cluster := support.ConnectCluster(t)
@@ -26,101 +25,103 @@ func TestTLSWithUserProvidedCertificate(t *testing.T) {
 	chartPath, err := filepath.Abs("..")
 	require.NoError(t, err)
 
-	support.WithNamespace(t, cluster, func(env *support.Env) {
-		env.Logger.Logf(t, "namespace %q created; installing PostgreSQL…", env.Namespace)
-		support.WithPostgres(t, env)
+	commonSetValues := map[string]string{
+		"zitadel.masterkey":                                         "x123456789012345678901234567891y",
+		"zitadel.configmapConfig.ExternalDomain":                    "zitadel.dev.mrida.ng",
+		"zitadel.configmapConfig.ExternalPort":                      "443",
+		"zitadel.configmapConfig.ExternalSecure":                    "true",
+		"zitadel.configmapConfig.Database.Postgres.Host":            "db-postgresql",
+		"zitadel.configmapConfig.Database.Postgres.Port":            "5432",
+		"zitadel.configmapConfig.Database.Postgres.Database":        "zitadel",
+		"zitadel.configmapConfig.Database.Postgres.MaxOpenConns":    "20",
+		"zitadel.configmapConfig.Database.Postgres.MaxIdleConns":    "10",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnLifetime": "30m",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnIdleTime": "5m",
+		"zitadel.configmapConfig.Database.Postgres.User.Username":   "postgres",
+		"zitadel.configmapConfig.Database.Postgres.User.SSL.Mode":   "disable",
+		"zitadel.configmapConfig.Database.Postgres.Admin.Username":  "postgres",
+		"zitadel.configmapConfig.Database.Postgres.Admin.SSL.Mode":  "disable",
+		"ingress.enabled":       "false",
+		"login.ingress.enabled": "false",
+		"service.appProtocol":   "kubernetes.io/https",
+		"service.port":          "443",
+	}
 
-		support.WithTLSSecret(t, env, "wildcard-tls", "*.dev.mrida.ng", "dev.mrida.ng")
-
-		releaseName := env.MakeRelease("zitadel-tls", "external-cert")
-
-		helmOptions := &helm.Options{
-			KubectlOptions: env.Kube,
-			SetValues: map[string]string{
-				"zitadel.masterkey":                                         "x123456789012345678901234567891y",
-				"zitadel.configmapConfig.ExternalDomain":                    "zitadel.dev.mrida.ng",
-				"zitadel.configmapConfig.ExternalPort":                      "443",
-				"zitadel.configmapConfig.ExternalSecure":                    "true",
-				"zitadel.configmapConfig.Database.Postgres.Host":            "db-postgresql",
-				"zitadel.configmapConfig.Database.Postgres.Port":            "5432",
-				"zitadel.configmapConfig.Database.Postgres.Database":        "zitadel",
-				"zitadel.configmapConfig.Database.Postgres.MaxOpenConns":    "20",
-				"zitadel.configmapConfig.Database.Postgres.MaxIdleConns":    "10",
-				"zitadel.configmapConfig.Database.Postgres.MaxConnLifetime": "30m",
-				"zitadel.configmapConfig.Database.Postgres.MaxConnIdleTime": "5m",
-				"zitadel.configmapConfig.Database.Postgres.User.Username":   "postgres",
-				"zitadel.configmapConfig.Database.Postgres.User.SSL.Mode":   "disable",
-				"zitadel.configmapConfig.Database.Postgres.Admin.Username":  "postgres",
-				"zitadel.configmapConfig.Database.Postgres.Admin.SSL.Mode":  "disable",
-				"zitadel.serverSslCrtSecret":                                "wildcard-tls",
-				"login.serverSslCrtSecret":                                  "wildcard-tls",
-				"ingress.enabled":                                           "false",
-				"login.ingress.enabled":                                     "false",
-				"service.appProtocol":                                       "kubernetes.io/https",
-				"service.port":                                              "443",
+	testCases := []struct {
+		name          string
+		setupTLS      func(t *testing.T, env *support.Env)
+		setValues     map[string]string
+		setJsonValues map[string]string
+	}{
+		{
+			name: "user-provided-certificate",
+			setupTLS: func(t *testing.T, env *support.Env) {
+				support.WithTLSSecret(t, env, "wildcard-tls", "*.dev.mrida.ng", "dev.mrida.ng")
 			},
-			SetJsonValues: map[string]string{
+			setValues: map[string]string{
+				"zitadel.serverSslCrtSecret": "wildcard-tls",
+				"login.serverSslCrtSecret":   "wildcard-tls",
+			},
+			setJsonValues: map[string]string{
 				"login.env": `[{"name":"NODE_TLS_REJECT_UNAUTHORIZED","value":"0"}]`,
 			},
-			ExtraArgs: map[string][]string{
-				"upgrade": {"--install", "--wait", "--timeout", "30m"},
+		},
+		{
+			name: "chart-generated-self-signed",
+			setupTLS: func(t *testing.T, env *support.Env) {
 			},
-		}
-
-		if err := helm.UpgradeE(t, helmOptions, chartPath, releaseName); err != nil {
-			dumpSetupAndInitJobLogs(t, env, releaseName)
-			require.NoError(t, err)
-		}
-
-		waitForDeploymentReady(t, env, releaseName)
-		waitForDeploymentReady(t, env, releaseName+"-login")
-
-		assertHTTPSAccessible(t, env, releaseName, 443)
-		assertHTTPSAccessible(t, env, releaseName+"-login", 443)
-	})
-}
-
-func dumpSetupAndInitJobLogs(t *testing.T, env *support.Env, releaseName string) {
-	namespace := env.Kube.Namespace
-	jobNames := []string{fmt.Sprintf("%s-setup", releaseName), fmt.Sprintf("%s-init", releaseName)}
-
-	for _, jobName := range jobNames {
-		labelSelector := fmt.Sprintf("job-name=%s", jobName)
-		pods := listPodsE(t, env, labelSelector)
-
-		for _, pod := range pods {
-			for _, container := range pod.Spec.Containers {
-				logOutput, _ := k8s.RunKubectlAndGetOutputE(
-					t,
-					env.Kube,
-					"logs",
-					pod.Name, "-n", namespace, "-c", container.Name, "--tail=500",
-				)
-				env.Logger.Logf(t, "---- logs: pod=%s container=%s ----\n%s\n---- end logs ----", pod.Name, container.Name, logOutput)
-			}
-			for _, initContainer := range pod.Spec.InitContainers {
-				logOutput, _ := k8s.RunKubectlAndGetOutputE(
-					t,
-					env.Kube,
-					"logs",
-					pod.Name, "-n", namespace, "-c", initContainer.Name, "--tail=500",
-				)
-				env.Logger.Logf(t, "---- logs: pod=%s initContainer=%s ----\n%s\n---- end logs ----", pod.Name, initContainer.Name, logOutput)
-			}
-		}
+			setValues: map[string]string{
+				"zitadel.selfSignedCert.enabled":           "true",
+				"zitadel.selfSignedCert.additionalDnsName": "zitadel.dev.mrida.ng",
+				"login.selfSignedCert.enabled":             "true",
+				"login.selfSignedCert.additionalDnsName":   "zitadel.dev.mrida.ng",
+			},
+			setJsonValues: map[string]string{
+				"login.env": `[{"name":"NODE_TLS_REJECT_UNAUTHORIZED","value":"0"}]`,
+			},
+		},
 	}
-}
 
-func listPodsE(t *testing.T, env *support.Env, labelSelector string) []corev1.Pod {
-	podList, err := env.Client.CoreV1().Pods(env.Kube.Namespace).List(
-		context.Background(),
-		metav1.ListOptions{LabelSelector: labelSelector},
-	)
-	if err != nil {
-		env.Logger.Logf(t, "warn: list pods selector=%q: %v", labelSelector, err)
-		return []corev1.Pod{}
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			support.WithNamespace(t, cluster, func(env *support.Env) {
+				env.Logger.Logf(t, "namespace %q created; installing PostgreSQL…", env.Namespace)
+				support.WithPostgres(t, env)
+
+				releaseName := env.MakeRelease("zitadel-tls", testCase.name)
+
+				testCase.setupTLS(t, env)
+
+				mergedSetValues := make(map[string]string)
+				for key, value := range commonSetValues {
+					mergedSetValues[key] = value
+				}
+				for key, value := range testCase.setValues {
+					mergedSetValues[key] = value
+				}
+
+				helmOptions := &helm.Options{
+					KubectlOptions: env.Kube,
+					SetValues:      mergedSetValues,
+					SetJsonValues:  testCase.setJsonValues,
+					ExtraArgs: map[string][]string{
+						"upgrade": {"--install", "--wait", "--timeout", "30m"},
+					},
+				}
+
+				require.NoError(t, helm.UpgradeE(t, helmOptions, chartPath, releaseName))
+
+				waitForDeploymentReady(t, env, releaseName)
+				waitForDeploymentReady(t, env, releaseName+"-login")
+
+				assertZitadelConsoleAccessible(t, env, releaseName, 443)
+				assertLoginUIAccessible(t, env, releaseName+"-login", 443)
+			})
+		})
 	}
-	return podList.Items
 }
 
 func waitForDeploymentReady(t *testing.T, env *support.Env, deploymentName string) {
@@ -136,7 +137,7 @@ func waitForDeploymentReady(t *testing.T, env *support.Env, deploymentName strin
 	}, 5*time.Minute, 5*time.Second)
 }
 
-func assertHTTPSAccessible(t *testing.T, env *support.Env, serviceName string, port int) {
+func assertZitadelConsoleAccessible(t *testing.T, env *support.Env, serviceName string, port int) {
 	t.Helper()
 
 	tunnel := k8s.NewTunnel(env.Kube, k8s.ResourceTypeService, serviceName, 0, port)
@@ -148,15 +149,91 @@ func assertHTTPSAccessible(t *testing.T, env *support.Env, serviceName string, p
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
 	}
 
 	require.Eventually(t, func() bool {
-		resp, err := client.Get(fmt.Sprintf("https://%s/healthz", tunnel.Endpoint()))
+		resp, err := client.Get(fmt.Sprintf("https://%s/ui/console", tunnel.Endpoint()))
 		if err != nil {
-			env.Logger.Logf(t, "health check failed for %s: %v", serviceName, err)
+			env.Logger.Logf(t, "console access failed for %s: %v", serviceName, err)
 			return false
 		}
 		defer resp.Body.Close()
+
+		env.Logger.Logf(t, "---- %s console response ----", serviceName)
+		env.Logger.Logf(t, "Status: %s", resp.Status)
+		env.Logger.Logf(t, "Headers:")
+		for key, values := range resp.Header {
+			for _, value := range values {
+				env.Logger.Logf(t, "  %s: %s", key, value)
+			}
+		}
+
+		body, _ := io.ReadAll(resp.Body)
+		bodyStr := string(body)
+		if len(bodyStr) > 1000 {
+			bodyStr = bodyStr[:1000] + "... (truncated)"
+		}
+		env.Logger.Logf(t, "Body: %s", bodyStr)
+		env.Logger.Logf(t, "---- end console response ----")
+
+		if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusFound || resp.StatusCode == http.StatusMovedPermanently {
+			if resp.StatusCode >= 300 && resp.StatusCode < 400 {
+				location := resp.Header.Get("Location")
+				env.Logger.Logf(t, "Console redirects to: %s", location)
+				return strings.Contains(location, "/ui/login") || strings.Contains(location, "/oauth")
+			}
+			return true
+		}
+
+		return false
+	}, 2*time.Minute, 5*time.Second)
+}
+
+func assertLoginUIAccessible(t *testing.T, env *support.Env, serviceName string, port int) {
+	t.Helper()
+
+	tunnel := k8s.NewTunnel(env.Kube, k8s.ResourceTypeService, serviceName, 0, port)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	require.Eventually(t, func() bool {
+		resp, err := client.Get(fmt.Sprintf("https://%s/ui/v2/login", tunnel.Endpoint()))
+		if err != nil {
+			env.Logger.Logf(t, "login UI access failed for %s: %v", serviceName, err)
+			return false
+		}
+		defer resp.Body.Close()
+
+		env.Logger.Logf(t, "---- %s login UI response ----", serviceName)
+		env.Logger.Logf(t, "Status: %s", resp.Status)
+		env.Logger.Logf(t, "Headers:")
+		for key, values := range resp.Header {
+			for _, value := range values {
+				env.Logger.Logf(t, "  %s: %s", key, value)
+			}
+		}
+
+		body, _ := io.ReadAll(resp.Body)
+		bodyStr := string(body)
+		if len(bodyStr) > 1000 {
+			bodyStr = bodyStr[:1000] + "... (truncated)"
+		}
+		env.Logger.Logf(t, "Body: %s", bodyStr)
+		env.Logger.Logf(t, "---- end login UI response ----")
+
 		return resp.StatusCode == http.StatusOK
 	}, 2*time.Minute, 5*time.Second)
 }
