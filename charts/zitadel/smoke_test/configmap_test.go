e72152fa8f088af638949fda810e46d5ddec0e88
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-10-13 16:20:50 +1100

    feat(tests): improved the test converage for other resources too

---
+++ charts/zitadel/smoke_test/configmap_test.go
@@ -0,0 +1,200 @@
+package smoke_test_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/zitadel/zitadel-charts/charts/zitadel/smoke_test/support"
+)
+
+type configMapExpected struct {
+	zitadelEnabled bool
+	loginEnabled   bool
+
+	zitadelAnnotations map[string]string
+	zitadelData        map[string]string
+
+	loginAnnotations map[string]string
+	loginData        map[string]string
+}
+
+func TestConfigMapMatrix(t *testing.T) {
+	t.Parallel()
+
+	cluster := support.ConnectCluster(t)
+
+	chartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+
+	commonSetValues := map[string]string{
+		"zitadel.masterkey":                                         "x123456789012345678901234567891y",
+		"zitadel.configmapConfig.ExternalDomain":                    "pg-insecure.127.0.0.1.sslip.io",
+		"zitadel.configmapConfig.ExternalPort":                      "443",
+		"zitadel.configmapConfig.TLS.Enabled":                       "false",
+		"zitadel.configmapConfig.Database.Postgres.Host":            "db-postgresql",
+		"zitadel.configmapConfig.Database.Postgres.Port":            "5432",
+		"zitadel.configmapConfig.Database.Postgres.Database":        "zitadel",
+		"zitadel.configmapConfig.Database.Postgres.MaxOpenConns":    "20",
+		"zitadel.configmapConfig.Database.Postgres.MaxIdleConns":    "10",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnLifetime": "30m",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnIdleTime": "5m",
+		"zitadel.configmapConfig.Database.Postgres.User.Username":   "postgres",
+		"zitadel.configmapConfig.Database.Postgres.User.SSL.Mode":   "disable",
+		"zitadel.configmapConfig.Database.Postgres.Admin.Username":  "postgres",
+		"zitadel.configmapConfig.Database.Postgres.Admin.SSL.Mode":  "disable",
+		"ingress.enabled":       "true",
+		"login.ingress.enabled": "true",
+	}
+
+	testCases := []struct {
+		name      string
+		setValues map[string]string
+		expected  configMapExpected
+	}{
+		{
+			name: "both-enabled-default",
+			setValues: map[string]string{
+				"login.enabled": "true",
+			},
+			expected: configMapExpected{
+				zitadelEnabled:     true,
+				loginEnabled:       true,
+				zitadelAnnotations: map[string]string{},
+				zitadelData: map[string]string{
+					"zitadel-config-yaml": "",
+				},
+				loginAnnotations: map[string]string{},
+				loginData: map[string]string{
+					".env": "",
+				},
+			},
+		},
+		{
+			name: "both-enabled-with-annotations",
+			setValues: map[string]string{
+				"configMap.annotations.owner":      "platform-team",
+				"login.enabled":                    "true",
+				"login.configMap.annotations.team": "frontend",
+			},
+			expected: configMapExpected{
+				zitadelEnabled: true,
+				loginEnabled:   true,
+				zitadelAnnotations: map[string]string{
+					"owner": "platform-team",
+				},
+				zitadelData: map[string]string{
+					"zitadel-config-yaml": "",
+				},
+				loginAnnotations: map[string]string{
+					"team": "frontend",
+				},
+				loginData: map[string]string{
+					".env": "",
+				},
+			},
+		},
+		{
+			name: "zitadel-only-login-disabled",
+			setValues: map[string]string{
+				"configMap.annotations.config-version": "v2",
+				"login.enabled":                        "false",
+			},
+			expected: configMapExpected{
+				zitadelEnabled: true,
+				loginEnabled:   false,
+				zitadelAnnotations: map[string]string{
+					"config-version": "v2",
+				},
+				zitadelData: map[string]string{
+					"zitadel-config-yaml": "",
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			support.WithNamespace(t, cluster, func(env *support.Env) {
+				env.Logger.Logf(t, "namespace %q created; installing PostgreSQL…", env.Namespace)
+				support.WithPostgres(t, env)
+
+				releaseName := env.MakeRelease("zitadel-test", testCase.name)
+
+				mergedSetValues := make(map[string]string)
+				for key, value := range commonSetValues {
+					mergedSetValues[key] = value
+				}
+				for key, value := range testCase.setValues {
+					mergedSetValues[key] = value
+				}
+
+				helmOptions := &helm.Options{
+					KubectlOptions: env.Kube,
+					SetValues:      mergedSetValues,
+					ExtraArgs: map[string][]string{
+						"upgrade": {"--install", "--wait", "--timeout", "30m"},
+					},
+				}
+
+				if err := helm.UpgradeE(t, helmOptions, chartPath, releaseName); err != nil {
+					//dumpSetupAndInitJobLogs(t, env, releaseName)
+					require.NoError(t, err)
+				}
+
+				var expectedZitadelConfigMap *corev1.ConfigMap
+				if testCase.expected.zitadelEnabled {
+					expectedZitadelConfigMap = &corev1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:        releaseName + "-config-yaml",
+							Annotations: testCase.expected.zitadelAnnotations,
+						},
+						Data: testCase.expected.zitadelData,
+					}
+				}
+				assertConfigMap(t, env, expectedZitadelConfigMap)
+
+				var expectedLoginConfigMap *corev1.ConfigMap
+				if testCase.expected.loginEnabled {
+					expectedLoginConfigMap = &corev1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:        releaseName + "-login-config-dotenv",
+							Annotations: testCase.expected.loginAnnotations,
+						},
+						Data: testCase.expected.loginData,
+					}
+				}
+				assertConfigMap(t, env, expectedLoginConfigMap)
+			})
+		})
+	}
+}
+
+func assertConfigMap(t *testing.T, env *support.Env, expected *corev1.ConfigMap) {
+	t.Helper()
+
+	if expected == nil {
+		return
+	}
+
+	actualConfigMap, err := env.Client.
+		CoreV1().
+		ConfigMaps(env.Kube.Namespace).
+		Get(context.Background(), expected.Name, metav1.GetOptions{})
+
+	require.NoError(t, err, "failed to get ConfigMap %s", expected.Name)
+	require.Equal(t, expected.Annotations, actualConfigMap.Annotations, "ConfigMap annotations mismatch for %s", expected.Name)
+
+	for key := range expected.Data {
+		_, exists := actualConfigMap.Data[key]
+		require.True(t, exists, "ConfigMap data key %s missing for %s", key, expected.Name)
+	}
+
+	env.Logger.Logf(t, "✓ Verified ConfigMap configuration for %s", expected.Name)
+}
---
+++ charts/zitadel/smoke_test/services_test.go
@@ -0,0 +1,372 @@
+package smoke_test_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"github.com/zitadel/zitadel-charts/charts/zitadel/smoke_test/support"
+)
+
+type serviceExpected struct {
+	zitadelEnabled bool
+	loginEnabled   bool
+
+	zitadelSpec        corev1.ServiceSpec
+	zitadelAnnotations map[string]string
+
+	loginSpec        corev1.ServiceSpec
+	loginAnnotations map[string]string
+}
+
+func TestServiceMatrix(t *testing.T) {
+	t.Parallel()
+
+	cluster := support.ConnectCluster(t)
+
+	chartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+
+	commonSetValues := map[string]string{
+		"zitadel.masterkey":                                         "x123456789012345678901234567891y",
+		"zitadel.configmapConfig.ExternalDomain":                    "pg-insecure.127.0.0.1.sslip.io",
+		"zitadel.configmapConfig.ExternalPort":                      "443",
+		"zitadel.configmapConfig.TLS.Enabled":                       "false",
+		"zitadel.configmapConfig.Database.Postgres.Host":            "db-postgresql",
+		"zitadel.configmapConfig.Database.Postgres.Port":            "5432",
+		"zitadel.configmapConfig.Database.Postgres.Database":        "zitadel",
+		"zitadel.configmapConfig.Database.Postgres.MaxOpenConns":    "20",
+		"zitadel.configmapConfig.Database.Postgres.MaxIdleConns":    "10",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnLifetime": "30m",
+		"zitadel.configmapConfig.Database.Postgres.MaxConnIdleTime": "5m",
+		"zitadel.configmapConfig.Database.Postgres.User.Username":   "postgres",
+		"zitadel.configmapConfig.Database.Postgres.User.SSL.Mode":   "disable",
+		"zitadel.configmapConfig.Database.Postgres.Admin.Username":  "postgres",
+		"zitadel.configmapConfig.Database.Postgres.Admin.SSL.Mode":  "disable",
+		"ingress.enabled":       "true",
+		"login.ingress.enabled": "true",
+	}
+
+	testCases := []struct {
+		name      string
+		setValues map[string]string
+		expected  serviceExpected
+	}{
+		{
+			name: "both-enabled-default-clusterip",
+			setValues: map[string]string{
+				"login.enabled": "true",
+			},
+			expected: serviceExpected{
+				zitadelEnabled: true,
+				loginEnabled:   true,
+				zitadelSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "start",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel",
+					},
+				},
+				zitadelAnnotations: map[string]string{},
+				loginSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "login",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel-login",
+					},
+				},
+				loginAnnotations: map[string]string{},
+			},
+		},
+		{
+			name: "both-enabled-custom-ports",
+			setValues: map[string]string{
+				"service.port":       "9090",
+				"login.enabled":      "true",
+				"login.service.port": "9091",
+			},
+			expected: serviceExpected{
+				zitadelEnabled: true,
+				loginEnabled:   true,
+				zitadelSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       9090,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "start",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel",
+					},
+				},
+				zitadelAnnotations: map[string]string{},
+				loginSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       9091,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "login",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel-login",
+					},
+				},
+				loginAnnotations: map[string]string{},
+			},
+		},
+		{
+			name: "both-enabled-loadbalancer",
+			setValues: map[string]string{
+				"service.type":                        "LoadBalancer",
+				"service.externalTrafficPolicy":       "Local",
+				"login.enabled":                       "true",
+				"login.service.type":                  "LoadBalancer",
+				"login.service.externalTrafficPolicy": "Cluster",
+			},
+			expected: serviceExpected{
+				zitadelEnabled: true,
+				loginEnabled:   true,
+				zitadelSpec: corev1.ServiceSpec{
+					Type:                  corev1.ServiceTypeLoadBalancer,
+					ExternalTrafficPolicy: corev1.ServiceExternalTrafficPolicyTypeLocal,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "start",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel",
+					},
+				},
+				zitadelAnnotations: map[string]string{},
+				loginSpec: corev1.ServiceSpec{
+					Type:                  corev1.ServiceTypeLoadBalancer,
+					ExternalTrafficPolicy: corev1.ServiceExternalTrafficPolicyTypeCluster,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "login",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel-login",
+					},
+				},
+				loginAnnotations: map[string]string{},
+			},
+		},
+		{
+			name: "both-enabled-with-annotations",
+			setValues: map[string]string{
+				"service.annotations.cloud\\.google\\.com/load-balancer-type": "Internal",
+				"service.annotations.owner":                                   "platform-team",
+				"login.enabled":                                               "true",
+				"login.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-internal": "true",
+			},
+			expected: serviceExpected{
+				zitadelEnabled: true,
+				loginEnabled:   true,
+				zitadelSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "start",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel",
+					},
+				},
+				zitadelAnnotations: map[string]string{
+					"cloud.google.com/load-balancer-type": "Internal",
+					"owner":                               "platform-team",
+				},
+				loginSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeClusterIP,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "login",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel-login",
+					},
+				},
+				loginAnnotations: map[string]string{
+					"service.beta.kubernetes.io/aws-load-balancer-internal": "true",
+				},
+			},
+		},
+		{
+			name: "zitadel-only-login-disabled",
+			setValues: map[string]string{
+				"service.type":  "NodePort",
+				"service.port":  "8888",
+				"login.enabled": "false",
+			},
+			expected: serviceExpected{
+				zitadelEnabled: true,
+				loginEnabled:   false,
+				zitadelSpec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8888,
+							Protocol:   corev1.ProtocolTCP,
+							TargetPort: intstr.FromInt32(8080),
+						},
+					},
+					Selector: map[string]string{
+						"app.kubernetes.io/component": "start",
+						"app.kubernetes.io/instance":  "",
+						"app.kubernetes.io/name":      "zitadel",
+					},
+				},
+				zitadelAnnotations: map[string]string{},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			support.WithNamespace(t, cluster, func(env *support.Env) {
+				env.Logger.Logf(t, "namespace %q created; installing PostgreSQL…", env.Namespace)
+				support.WithPostgres(t, env)
+
+				releaseName := env.MakeRelease("zitadel-test", testCase.name)
+
+				mergedSetValues := make(map[string]string)
+				for key, value := range commonSetValues {
+					mergedSetValues[key] = value
+				}
+				for key, value := range testCase.setValues {
+					mergedSetValues[key] = value
+				}
+
+				helmOptions := &helm.Options{
+					KubectlOptions: env.Kube,
+					SetValues:      mergedSetValues,
+					ExtraArgs: map[string][]string{
+						"upgrade": {"--install", "--wait", "--timeout", "30m"},
+					},
+				}
+
+				if err := helm.UpgradeE(t, helmOptions, chartPath, releaseName); err != nil {
+					//dumpSetupAndInitJobLogs(t, env, releaseName)
+					require.NoError(t, err)
+				}
+
+				var expectedZitadelService *corev1.Service
+				if testCase.expected.zitadelEnabled {
+					zitadelAnnotations := map[string]string{
+						"meta.helm.sh/release-name":      releaseName,
+						"meta.helm.sh/release-namespace": env.Namespace,
+					}
+					for k, v := range testCase.expected.zitadelAnnotations {
+						zitadelAnnotations[k] = v
+					}
+
+					zitadelSpec := testCase.expected.zitadelSpec
+					zitadelSpec.Selector["app.kubernetes.io/instance"] = releaseName
+
+					expectedZitadelService = &corev1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:        releaseName,
+							Annotations: zitadelAnnotations,
+						},
+						Spec: zitadelSpec,
+					}
+				}
+				assertService(t, env, expectedZitadelService)
+
+				var expectedLoginService *corev1.Service
+				if testCase.expected.loginEnabled {
+					loginAnnotations := map[string]string{
+						"meta.helm.sh/release-name":      releaseName,
+						"meta.helm.sh/release-namespace": env.Namespace,
+					}
+					for k, v := range testCase.expected.loginAnnotations {
+						loginAnnotations[k] = v
+					}
+
+					loginSpec := testCase.expected.loginSpec
+					loginSpec.Selector["app.kubernetes.io/instance"] = releaseName
+
+					expectedLoginService = &corev1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:        releaseName + "-login",
+							Annotations: loginAnnotations,
+						},
+						Spec: loginSpec,
+					}
+				}
+				assertService(t, env, expectedLoginService)
+			})
+		})
+	}
+}
+
+func assertService(t *testing.T, env *support.Env, expected *corev1.Service) {
+	t.Helper()
+
+	if expected == nil {
+		return
+	}
+
+	actualService, err := env.Client.
+		CoreV1().
+		Services(env.Kube.Namespace).
+		Get(context.Background(), expected.Name, metav1.GetOptions{})
+
+	require.NoError(t, err, "failed to get Service %s", expected.Name)
+	require.Equal(t, expected.Spec, actualService.Spec, "Service spec mismatch for %s", expected.Name)
+	require.Equal(t, expected.Annotations, actualService.Annotations, "Service annotations mismatch for %s", expected.Name)
+
+	env.Logger.Logf(t, "✓ Verified Service configuration for %s", expected.Name)
+}
