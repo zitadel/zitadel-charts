ce81fa26fcd83d7a23ac0a90b8dcb21f2a59877a
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-10-22 15:21:56 +1100

    used knip to find all missing dependencies

--- charts/zitadel/acceptance_test/setup_test.go
+++ charts/zitadel/acceptance_test/setup_test.go
@@ -4,33 +4,12 @@ import (
 	"strings"
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
-	"github.com/jinzhu/copier"
 	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 func (s *ConfigurationTest) SetupTest() {
-	clusterKubectl := new(k8s.KubectlOptions)
 	t := s.T()
-	if err := copier.Copy(clusterKubectl, s.KubeOptions); err != nil {
-		t.Fatal(err)
-	}
-	clusterKubectl.Namespace = ""
-	if err := k8s.KubectlDeleteFromStringE(t, clusterKubectl, `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: crdb
-`); err != nil && !isNotFoundFromKubectl(err) {
-		t.Fatal(err)
-	}
-	if err := k8s.KubectlDeleteFromStringE(t, clusterKubectl, `
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: crdb
-`); err != nil && !isNotFoundFromKubectl(err) {
-		t.Fatal(err)
-	}
+
 	_, err := k8s.GetNamespaceE(t, s.KubeOptions, s.KubeOptions.Namespace)
 	notFound := errors.IsNotFound(err)
 	if err != nil && !notFound {
