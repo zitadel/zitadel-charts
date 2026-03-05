53e33ceb4f3c205473d2344d004d43a579d47f77
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-03-05 14:50:11 +1100

    refactor: use table-driven pattern for ingress nginx controller test

--- test/smoke/ingress_test.go
+++ test/smoke/ingress_test.go
@@ -55,30 +55,48 @@ func TestIngressNginxControllerAnnotation(t *testing.T) {
 	chartPath := support.ChartPath(t)
 	releaseName := "ingress-nginx"
 
-	render := func(controller string) networkingv1.Ingress {
-		options := &helm.Options{
-			SetValues: map[string]string{
-				"image.tag":                              support.DigestTag,
-				"ingress.enabled":                        "true",
-				"ingress.controller":                     controller,
-				"ingress.hosts[0].host":                  "zitadel.example.local",
-				"zitadel.configmapConfig.ExternalDomain": "zitadel.example.local",
-				"zitadel.masterkey":                      "01234567890123456789012345678901",
+	testCases := []struct {
+		name                string
+		controller          string
+		expectedAnnotations map[string]string
+	}{
+		{
+			name:       "nginx controller injects backend-protocol annotation",
+			controller: "nginx",
+			expectedAnnotations: map[string]string{
+				"nginx.ingress.kubernetes.io/backend-protocol": "GRPC",
 			},
-		}
-		rendered := helm.RenderTemplate(t, options, chartPath, releaseName, []string{"templates/ingress_zitadel.yaml"})
-		var ingress networkingv1.Ingress
-		helm.UnmarshalK8SYaml(t, rendered, &ingress)
-		return ingress
+		},
+		{
+			name:                "generic controller omits backend-protocol annotation",
+			controller:          "generic",
+			expectedAnnotations: map[string]string{},
+		},
 	}
 
-	t.Run("nginx controller injects backend-protocol annotation", func(t *testing.T) {
-		ingress := render("nginx")
-		assert.Equal(t, "GRPC", ingress.Annotations["nginx.ingress.kubernetes.io/backend-protocol"])
-	})
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			options := &helm.Options{
+				SetValues: map[string]string{
+					"image.tag":                              support.DigestTag,
+					"ingress.enabled":                        "true",
+					"ingress.controller":                     tc.controller,
+					"ingress.hosts[0].host":                  "zitadel.example.local",
+					"zitadel.configmapConfig.ExternalDomain": "zitadel.example.local",
+					"zitadel.masterkey":                      "01234567890123456789012345678901",
+				},
+			}
+			rendered := helm.RenderTemplate(t, options, chartPath, releaseName, []string{"templates/ingress_zitadel.yaml"})
+			var ingress networkingv1.Ingress
+			helm.UnmarshalK8SYaml(t, rendered, &ingress)
 
-	t.Run("generic controller omits backend-protocol annotation", func(t *testing.T) {
-		ingress := render("generic")
-		assert.NotContains(t, ingress.Annotations, "nginx.ingress.kubernetes.io/backend-protocol")
-	})
+			for key, value := range tc.expectedAnnotations {
+				assert.Equal(t, value, ingress.Annotations[key])
+			}
+			for key := range ingress.Annotations {
+				assert.Contains(t, tc.expectedAnnotations, key, "unexpected annotation %q", key)
+			}
+		})
+	}
 }
